36063f0cb0096f5ba306797f1f462fd286fde1ba
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-01-26 17:19:40 +0530

    Update arrayqns.java

--- arrayqns.java
+++ arrayqns.java
@@ -2,17 +2,27 @@ package com.iflak;
 
 public class arrayqns {
     public static void main(String[] args) {
-        int arr[]={2,3,4,5,6,7};
-        int target=8;
-        sol(arr,target);
+        int arr[] = {2, 3, 4, 5, 6, 7};
+        int target = 8;
+        int[] result = sol(arr, target);
+
+        if (result != null) {
+            System.out.println("Pair found: " + result[0] + " " + result[1]);
+        } else {
+            System.out.println("Pair not found");
+        }
     }
-    public static void sol(int[] arr, int target){
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==target){
-                    System.out.println(arr[i]+" "+arr[j]);
+
+    public static int[] sol(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{arr[i], arr[j]};
                 }
             }
         }
+
+        // Return null if no pair is found
+        return null;
     }
 }
