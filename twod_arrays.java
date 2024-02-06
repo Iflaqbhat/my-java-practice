e3c61cacaf33e80fd29e5fad7994c0f1c40650bb
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-02-06 10:18:56 +0530

    Update twod_arrays.java

--- twod_arrays.java
+++ twod_arrays.java
@@ -6,34 +6,34 @@ import java.util.Scanner;
 public class twod_arrays {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // int[][] arr=new int[4][];
-        //arr[1][1]=44;
-        //
-        // int[][] arr2= {
-        //     {1,2,3},
-        //   {4,5,6},
-        // {7,8}
-//        };
-//        System.out.println(arr2[9][0]);
 
         int[][] arr3 = new int[3][3];
-        System.out.println(arr3.length);
-        //input
+
+        // Input
         for (int row = 0; row < arr3.length; row++) {
             for (int col = 0; col < arr3[row].length; col++) {
                 arr3[row][col] = in.nextInt();
             }
-
         }
-//        for (int row = 0; row < arr3.length; row++) {
-//            for (int col = 0; col < arr3[row].length; col++) {
-//
-//                System.out.print(arr3[col][row]+" ");
-//            }
-//            System.out.println();
-//        }
-        for (int row=0;row< arr3.length;row++){
+
+        // Print original array
+        System.out.println("Original Array:");
+        for (int row = 0; row < arr3.length; row++) {
             System.out.println(Arrays.toString(arr3[row]));
+        }
+
+        // Transpose the array
+        int[][] transposedArray = new int[arr3[0].length][arr3.length];
+        for (int row = 0; row < arr3.length; row++) {
+            for (int col = 0; col < arr3[row].length; col++) {
+                transposedArray[col][row] = arr3[row][col];
+            }
+        }
+
+        // Print transposed array
+        System.out.println("Transposed Array:");
+        for (int row = 0; row < transposedArray.length; row++) {
+            System.out.println(Arrays.toString(transposedArray[row]));
         }
     }
 }
